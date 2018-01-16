cbf01a77608ffe64e51424fd2434d0e77095237c
Souheil CHELFOUH <trollfot@gmail.com>
2018-01-16 20:30:10 +0100

    Added comments to app.py

--- app.py
+++ app.py
@@ -15,24 +15,32 @@ from crom import monkey, implicit
 monkey.incompat()
 implicit.initialize()
 
+# Read the ZCML
+# This is not a mandatory stage.
+# Actually, what the ZCML loading does is grok the packages
+# This can be done manually, using `crom.configure`
+from cromlech.configuration.utils import load_zcml
+load_zcml(os.path.join(HERE, 'app.zcml'))
+
+# Load translation
+# This is needed only if we want internationalization
+from cromlech.i18n import load_translations_directories
+load_translations_directories()
+
+# Adding the event dispatcher
+# Use only if you have event handlers or plan to have some.
+from cromlech.events import setup_dispatcher
+setup_dispatcher()
+
 # Getting the crypto key and creating the JWT service
+# We chose the JWT signed cookie for the demo
+# `cromlech.sessions` proposed different backends.
+# Or you can use the WSGI session middleware of your choice.
 from cromlech.sessions.jwt import key_from_file
 from cromlech.sessions.jwt import JWTCookieSession
 
 key = key_from_file(os.path.join(HERE, 'jwt.key'))
 session_wrapper = JWTCookieSession(key, 300)
-
-# read the ZCML
-from cromlech.configuration.utils import load_zcml
-load_zcml(os.path.join(HERE, 'app.zcml'))
-
-# load translation
-from cromlech.i18n import load_translations_directories
-load_translations_directories()
-
-# Adding the event dispatcher
-from cromlech.events import setup_dispatcher
-setup_dispatcher()
 
 # Create the application, including the middlewares.
 from cromdemo.wsgi import demo_application
